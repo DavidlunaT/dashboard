6b65e83453ab79be7b7713755848e22d5cd34d8d
David Andrés Luna Tenecela <136658235+DavidlunaT@users.noreply.github.com>
2024-12-03 00:25:37 +0000

    se completo todo de la guia

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,97 @@ import IndicatorWeather from './components/IndicatorWeather';
 import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
 import LineChartWeather from './components/LineChartWeather';
+import Item from './interface/Item.tsx';
+ {/* Hooks */ }
+ import { useEffect, useState } from 'react';
+let items: "";
+
+function setItems(items){
+
+}
+ interface Indicator {
+  title?: String;
+  subtitle?: String;
+  value?: String;
+}
 
 function App() {
+   {/* Variable de estado y función de actualización */}
+   let [indicators, setIndicators] = useState<Indicator[]>([])
+   let [owm, setOWM] = useState(localStorage.getItem("openWeatherMap"))
+
+   {/* Hook: useEffect */}
+   useEffect(()=>{
+
+    let request = async () => {
+        
+        {/* Request */}
+        let API_KEY = "d7de925e3450e1686dfb3453763173e6"
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+        let savedTextXML = await response.text();
+         {/* XML Parser */}
+         const parser = new DOMParser();
+         const xml = parser.parseFromString(savedTextXML, "application/xml");
+         {/* Arreglo para agregar los resultados */}
+
+         let dataToIndicators : Indicator[] = new Array<Indicator>();
+
+         {/* 
+             Análisis, extracción y almacenamiento del contenido del XML 
+             en el arreglo de resultados
+         */}
+
+         let name = xml.getElementsByTagName("name")[0].innerHTML || ""
+         dataToIndicators.push({"title":"Location", "subtitle": "City", "value": name})
+
+         let location = xml.getElementsByTagName("location")[1]
+
+         let latitude = location.getAttribute("latitude") || ""
+         dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+
+         let longitude = location.getAttribute("longitude") || ""
+         dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+
+         let altitude = location.getAttribute("altitude") || ""
+         dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+
+         
+        // console.log( dataToIndicators )
+
+        {/* Modificación de la variable de estado mediante la función de actualización */}
+        setIndicators( dataToIndicators )
+    }
+
+    request();
+
+},[])
+let renderIndicators = () => {
+
+  return indicators
+          .map(
+              (indicator, idx) => (
+                  <Grid key={idx} size={{ xs: 12, xl: 3 }}>
+                      <IndicatorWeather 
+                          title={indicator["title"]} 
+                          subtitle={indicator["subtitle"]} 
+                          value={indicator["value"]} />
+                  </Grid>
+              )
+          )
+   
+}
+
   return (
     <Grid container spacing={5}>
-
+      
         {/* Indicadores */}
+        {/*
         <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /></Grid>
         <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} /></Grid>
         <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} /> </Grid>
         <Grid size={{ xs: 12, xl: 3 }}> <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} /></Grid>
-       
+       */}
+       {renderIndicators()}
         {/* Tabla */}
         <Grid size={{ xs: 12, xl: 8 }}>
           {/* Grid Anidado */}
--- src/components/ControlWeather.tsx
+++ src/components/ControlWeather.tsx
@@ -6,9 +6,31 @@ import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-   
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+ {/* Hooks */ }
+ import { useState, useRef } from 'react';
+
+
 export default function ControlWeather() {
+    {/* Variable de estado y función de actualización */}
+    let [selected, setSelected] = useState(-1)
+{/* Constante de referencia a un elemento HTML */ }
+const descriptionRef = useRef<HTMLDivElement>(null);
+
+{/* Manejador de eventos */}
+const handleChange = (event: SelectChangeEvent) => {
+
+    let idx = parseInt(event.target.value)
+    // alert( idx );
+    setSelected( idx );
+
+    {/* Modificación de la referencia descriptionRef */}
+    if (descriptionRef.current !== null) {
+        descriptionRef.current.innerHTML = (idx >= 0) ? items[idx]["description"] : ""
+    }
+
+};
 
     {/* Arreglo de objetos */}
     let items = [
@@ -20,6 +42,8 @@ export default function ControlWeather() {
     {/* Arreglo de elementos JSX */}
     let options = items.map( (item, key) => <MenuItem key={key} value={key}>{item["name"]}</MenuItem> )
        
+  
+
     {/* JSX */}
     return (
         <Paper
@@ -29,21 +53,21 @@ export default function ControlWeather() {
                 flexDirection: 'column'
             }}
         >
-
-            <Typography mb={2} component="h3" variant="h6" color="primary">
-                Variables Meteorológicas
-            </Typography>
-
+{/* Use la variable de estado para renderizar del item seleccionado */}
+             
+<Typography ref={descriptionRef} mt={2} component="p" color="text.secondary" />
+		
             <Box sx={{ minWidth: 120 }}>
                    
                 <FormControl fullWidth>
                     <InputLabel id="simple-select-label">Variables</InputLabel>
                     <Select
-                        labelId="simple-select-label"
-                        id="simple-select"
-                        label="Variables"
-                        defaultValue='-1'
-                    >
+             labelId="simple-select-label"
+             id="simple-select"
+             label="Variables"
+             defaultValue='-1'
+             onChange={handleChange}
+         >
                         <MenuItem key="-1" value="-1" disabled>Seleccione una variable</MenuItem>
 
                         {options}
@@ -52,7 +76,6 @@ export default function ControlWeather() {
                 </FormControl>
 
             </Box>
-
 
         </Paper>
 
