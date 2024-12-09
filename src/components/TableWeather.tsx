23d42fcd7f888527e35b50f53b4f2293c8a8f351
David Andrés Luna Tenecela <136658235+DavidlunaT@users.noreply.github.com>
2024-12-09 15:18:26 +0000

    Se corrigo el contenido como está en la guía

--- src/components/TableWeather.tsx
+++ src/components/TableWeather.tsx
@@ -5,38 +5,42 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Item from '../interface/Item'; // Importa la interfaz Item
+import { useEffect, useState } from 'react';
+import Item from '../interface/Item';  // Asegúrate de importar la interfaz Item correctamente
 
-interface TableWeatherProps {
-  itemsIn: Item[]; // Define la propiedad itemsIn como un arreglo de Item
+interface MyProp {
+  itemsIn: Item[];
 }
 
-export default function TableWeather({ itemsIn }: TableWeatherProps) {
+export default function BasicTable(props: MyProp) {
+  const [rows, setRows] = useState<Item[]>([]);
+
+  useEffect(() => {
+    setRows(props.itemsIn);  // Actualiza los rows cuando cambian los props
+  }, [props.itemsIn]);
+
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Start Date</TableCell>
-            <TableCell align="right">End Date</TableCell>
-            <TableCell align="right">Precipitation</TableCell>
-            <TableCell align="right">Humidity</TableCell>
-            <TableCell align="right">Clouds</TableCell>
+            <TableCell>Hora de inicio</TableCell>
+            <TableCell align="right">Hora de fin</TableCell>
+            <TableCell align="right">Precipitación</TableCell>
+            <TableCell align="right">Humedad</TableCell>
+            <TableCell align="right">Nubosidad</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {itemsIn.map((item, index) => (
-            <TableRow
-              key={index}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
+          {rows.map((row, idx) => (
+            <TableRow key={idx} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               <TableCell component="th" scope="row">
-                {item.dateStart}
+                {row.dateStart}  {/* Hora de inicio */}
               </TableCell>
-              <TableCell align="right">{item.dateEnd}</TableCell>
-              <TableCell align="right">{item.precipitation}</TableCell>
-              <TableCell align="right">{item.humidity}</TableCell>
-              <TableCell align="right">{item.clouds}</TableCell>
+              <TableCell align="right">{row.dateEnd}</TableCell>  {/* Hora de fin */}
+              <TableCell align="right">{row.precipitation}</TableCell>  {/* Precipitación */}
+              <TableCell align="right">{row.humidity}</TableCell>  {/* Humedad */}
+              <TableCell align="right">{row.clouds}</TableCell>  {/* Nubosidad */}
             </TableRow>
           ))}
         </TableBody>
